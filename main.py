34e71a86fb6a042489abf50e5b820349f87547c4
Marat Faizerakhmanov <maratismodest@gmail.com>
2023-08-29 02:05:55 +0300

    main voice

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
-# V3
+# V4
 import os
 import torch
-import torchaudio
 
 device = torch.device('cpu')
 torch.set_num_threads(4)
 local_file = 'model.pt'
 
 if not os.path.isfile(local_file):
-    torch.hub.download_url_to_file('https://models.silero.ai/models/tts/tt/v3_tt.pt', local_file)
+    torch.hub.download_url_to_file('https://models.silero.ai/models/tts/cyr/v4_cyrillic.pt',
+                                   local_file)
 
 model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
 model.to(device)
 
-example_text = 'Мин почти кеше тавышы. Мин сезнең белән д+ус булырга телим'
-
-#җ
-#ү
-#ә
-#һ
-#ң
-text=example_text
-filename=text+'.mp3'
-
+example_text = """<speak><p>
+                                      <s>Мин почти кеше тавышы.</s>
+                                      <s>Мин сезнең белән д+ус булырга телим.</s>
+                                       <s>Минем дустым Америкадан.</s>
+                                    </p></speak>"""
 sample_rate = 48000
-speaker='dilyara'
-put_accent=True
+speaker='marat_tt'
 
-audio_paths = model.apply_tts(text=text+'.',
-                        speaker=speaker,
-                        sample_rate=sample_rate,
-                        put_accent=put_accent,
-                        )
-
-torchaudio.save('audio/' + filename,
-                  audio_paths.unsqueeze(0),
-                  sample_rate=sample_rate)
+audio_paths = model.save_wav(text=example_text,
+                             speaker=speaker,
+                             sample_rate=sample_rate)
