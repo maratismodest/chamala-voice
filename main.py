4b2953228259f997e81f6de6d781f0885f6ce6f9
Marat Faizerakhmanov <maratismodest@gmail.com>
2025-04-29 14:35:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ if __name__ == '__main__':
 
     # Get port from environment variable for Railway deployment
     # or use default 80 for production
-    port = int(os.environ.get("PORT", 80))
+    port = int(os.environ.get("PORT", 8000))
 
     uvicorn.run(app, host="0.0.0.0", port=port, timeout_keep_alive=10)
     # uvicorn.run("main:app", host="0.0.0.0", port=port, timeout_keep_alive=10, reload=True)
