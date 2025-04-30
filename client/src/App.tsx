067fd3ee0d5cd450009edc54ca8811f2e9ba1db2
Marat Faizerakhmanov <maratismodest@gmail.com>
2025-04-30 21:44:30 +0400

    add yandex metrika

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 import axios from 'axios';
 // import {tatarLetters} from "./shared/constants";
 import {SubmitHandler, useForm} from "react-hook-form";
+import {Spinner} from "./shared/ui/Spinner.tsx";
 
 type Inputs = {
     text: string
@@ -47,26 +48,42 @@ function App() {
 
     return (
         <>
-            <h1 className='text-3xl'>Tatar Text-to-Speech</h1>
-            <form
-                className='mt-4'
-                onSubmit={handleSubmit(onSubmit)}>
-                <textarea placeholder="Enter Tatar text here..." rows={6} autoFocus required
-                          {...register("text")}></textarea>
-                {/*<ul className='flex gap-3 mt-2 justify-center'>*/}
-                {/*    {tatarLetters.map((button, index) => (*/}
-                {/*        <li key={index}>*/}
-                {/*            <button type="button" onClick={() => setValue('text', text + button)}>{button}</button>*/}
-                {/*        </li>*/}
-                {/*    ))}*/}
-                {/*</ul>*/}
-                <button type="submit" disabled={loading} className='mt-8 !px-12 !py-4 mx-auto'>Convert to Speech
-                </button>
-            </form>
+            <header>
+                <h1 className='text-3xl'>Tatar Text-to-Speech</h1>
+            </header>
+            <main>
+                <form
+                    className='mt-4'
+                    onSubmit={handleSubmit(onSubmit)}>
+                <textarea
+                    placeholder="Enter Tatar text here..."
+                    rows={6}
+                    autoFocus
+                    required
+                    {...register("text")}
+                />
+                    {/*<ul className='flex gap-3 mt-2 justify-center'>*/}
+                    {/*    {tatarLetters.map((button, index) => (*/}
+                    {/*        <li key={index}>*/}
+                    {/*            <button type="button" onClick={() => setValue('text', text + button)}>{button}</button>*/}
+                    {/*        </li>*/}
+                    {/*    ))}*/}
+                    {/*</ul>*/}
+                    <div className='h-8 flex items-center justify-center'>
+                        {loading && <Spinner/>}
+                    </div>
+                    <button type="submit" disabled={loading}
+                            className='mt-2 !px-12 !py-4 mx-auto'>
 
-            <div className='mt-5'>
-                <audio id="audioPlayer" controls className='hidden mx-auto'></audio>
-            </div>
+                        Convert to Speech
+                    </button>
+                </form>
+
+                <div className='mt-5'>
+                    <audio id="audioPlayer" controls className='hidden mx-auto'></audio>
+                </div>
+            </main>
+            <footer className='mt-auto'>@2025 Marat Faizerakhmanov</footer>
         </>
     )
 }
